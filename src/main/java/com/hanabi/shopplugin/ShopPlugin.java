17cd51b0061782a9993e8f960003f09e8b1cbc8f
hanabi2528 <hanabi191211@gmail.com>
2024-03-09 01:16:08 +0900

    9*6のインベントリの中身の作成（man10風） インベントリの中身を動かせないようにした 看板の編集をできないようにした

--- src/main/java/com/hanabi/shopplugin/ShopPlugin.java
+++ src/main/java/com/hanabi/shopplugin/ShopPlugin.java
@@ -9,19 +9,71 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerSignOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ShopPlugin extends JavaPlugin implements Listener {
 
+    private Inventory createCustomInventory(Player player) {
+        Inventory inventory = Bukkit.createInventory(null, 9 * 6, "買取ショップ");
+
+        // 1列目
+        ItemStack blueGlass = createItem(Material.BLUE_STAINED_GLASS, "");
+        setInventoryColumn(inventory, blueGlass, 0);
+
+        // 2列目
+        ItemStack diamond = createItem(Material.DIAMOND, "ダイヤモンド");
+        setInventoryColumn(inventory, blueGlass, 1);
+        inventory.setItem(1 * 9 + 4, diamond);
+        setInventoryColumn(inventory, blueGlass, 2);
+
+        // 3列目
+        setInventoryColumn(inventory, blueGlass, 2);
+
+        // 4列目
+        setInventoryColumn(inventory, blueGlass, 3);
+        setInventoryColumn(inventory, createItem(Material.RED_STAINED_GLASS, "売却"), 4);
+        setInventoryColumn(inventory, blueGlass, 5);
+
+        // 5列目
+        setInventoryColumn(inventory, blueGlass, 4);
+        inventory.setItem(4 * 9 + 1, createItem(Material.RED_BANNER, "一つ減らす"));
+        setInventoryColumn(inventory, createItem(Material.RED_STAINED_GLASS, "売却"), 4);
+        inventory.setItem(4 * 9 + 7, createItem(Material.BLUE_BANNER, "一つ増やす"));
+        setInventoryColumn(inventory, blueGlass, 5);
+
+        // 6列目
+        setInventoryColumn(inventory, blueGlass, 5);
+
+        player.openInventory(inventory);
+        return inventory;
+    }
+
+    // 指定されたアイテムで列を埋めるメソッド
+    private void setInventoryColumn(Inventory inventory, ItemStack item, int column) {
+        for (int row = 0; row < inventory.getSize() / 9; row++) {
+            inventory.setItem(row * 9 + column, item);
+        }
+    }
+
+    // アイテムスタックを作成するメソッド
+    private ItemStack createItem(Material material, String displayName) {
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(displayName);
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("Shopプラグインが開始しました");
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
         // Plugin startup logic
 
     }
@@ -31,8 +83,8 @@ public final class ShopPlugin extends JavaPlugin implements Listener {
         Bukkit.getLogger().info("Shopプラグインが終了しました");
         // Plugin shutdown logic
     }
-
-    public void onPlayerSignOpenEvent(PlayerSignOpenEvent event){
+    @EventHandler
+    public void onPlayerSignOpenEvent(PlayerSignOpenEvent event) {
         event.setCancelled(true);
     }
 
@@ -42,7 +94,7 @@ public final class ShopPlugin extends JavaPlugin implements Listener {
         Action action = event.getAction();
 
         // 左クリックかつ対象がブロックの場合
-        if (action == Action.LEFT_CLICK_BLOCK) {
+        if (action == Action.RIGHT_CLICK_BLOCK) {
             Block clickedBlock = event.getClickedBlock();
 
             // 対象が看板の場合
@@ -50,9 +102,9 @@ public final class ShopPlugin extends JavaPlugin implements Listener {
                 Sign sign = (Sign) clickedBlock.getState();
                 String[] lines = sign.getLines();
 
-                if (lines[0].equalsIgnoreCase("公式ショップ")){
-                    if (lines[1].equalsIgnoreCase("ダイヤモンド")){
-                        opendiamondInventory(player);
+                if (lines[0].equalsIgnoreCase("買取ショップ")) {
+                    if (lines[1].equalsIgnoreCase("ダイヤモンド")) {
+                        createCustomInventory(player);
                     }
                 }
 
@@ -60,15 +112,13 @@ public final class ShopPlugin extends JavaPlugin implements Listener {
             }
         }
     }
-
-
-    private void opendiamondInventory(Player player){
-        Inventory inventory = getServer().createInventory(null,9,"Shopインベントリ");
-        ItemStack itemStack = new ItemStack(Material.DIAMOND);
-        inventory.addItem(itemStack);
-        player.openInventory(inventory);
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getClickedInventory() != null && event.getClickedInventory().equals("買取ショップ")) {
+            // プレイヤーがカスタムインベントリ内でアイテムを移動しようとした場合
+            event.setCancelled(true); // アイテムの移動をキャンセル
+        }
     }
-
 }
 
 
